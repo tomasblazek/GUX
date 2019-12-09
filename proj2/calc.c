251d10fff22d37ee06b21985392f4500257cc098
Unknown <tblazek33@gmail.com>
2019-12-09 02:49:25 +0100

    Extended math operations

--- proj2/calc.c
+++ proj2/calc.c
@@ -1,7 +1,7 @@
 #include <gtk/gtk.h>
 #include <gdk/gdkkeysyms.h>
 #include <stdbool.h>
-#include <ctype.h>
+#include <math.h>
 
 
 #define MIN_WIDHT 200
@@ -10,11 +10,17 @@
 #define MAX_HEIGHT 450
 #define SIZE_OF_BUFFER 1000
 
+
+enum Arity{CONSTANT, UNARY, BINARY};
+enum Mode{BASIC, SCIENTIFIC, PROGRAMMER} mode = BASIC;
+
 typedef struct Keys {
     GtkWidget *button;
     gchar *label;
     gint gdk_key;
     gint gdk_key2;
+    enum Mode mode;
+    enum Arity arity;
     guint x1;
     guint x2;
     guint y1;
@@ -22,63 +28,56 @@ typedef struct Keys {
 } Key;
 
 // my keys
-#define KEY_UNDECLARED (-1)
-#define KEY_CLEAR (-2)
-#define KEY_NEGATIVE (-1)
+#define KEY_UNDECLARED  (-1)
+#define KEY_CLEAR       (-2)
+#define KEY_NEGATIVE    (-3)
+#define KEY_FACTORIAL   (-4)
+#define KEY_POWER       (-5)
+#define KEY_SQRT        (-6)
+#define KEY_LOG         (-7)
+#define KEY_AND         (-8)
+#define KEY_OR          (-9)
+#define KEY_NOT         (-10)
+#define KEY_XOR         (-11)
 
 
 #define NUM_ROW_KEY_BASIC 5
 #define NUM_COLUMN_KEY_BASIC 4
-Key basic_buttons[] = {
-        {NULL, "↩",     GDK_KEY_BackSpace,  KEY_UNDECLARED, 0, 1, 0, 1},
-        {NULL, "CE",    KEY_CLEAR,          KEY_UNDECLARED, 1, 2, 0, 1},
-        {NULL, "(-1)",  KEY_NEGATIVE,       KEY_UNDECLARED, 3, 4, 0, 1},
-        {NULL, "1",     GDK_KEY_1,          GDK_KEY_KP_1, 0, 1, 1, 2},
-        {NULL, "2",     GDK_KEY_2,          GDK_KEY_KP_2, 1, 2, 1, 2},
-        {NULL, "3",     GDK_KEY_3,          GDK_KEY_KP_3, 2, 3, 1, 2},
-        {NULL, "+",     GDK_KEY_plus,       GDK_KEY_KP_Add, 3, 4, 1, 2},
-        {NULL, "4",     GDK_KEY_4,          GDK_KEY_KP_4, 0, 1, 2, 3},
-        {NULL, "5",     GDK_KEY_5,          GDK_KEY_KP_5, 1, 2, 2, 3},
-        {NULL, "6",     GDK_KEY_6,          GDK_KEY_KP_6, 2, 3, 2, 3},
-        {NULL, "-",     GDK_KEY_minus,      GDK_KEY_KP_Subtract, 3, 4, 2, 3},
-        {NULL, "7",     GDK_KEY_7,          GDK_KEY_KP_7, 0, 1, 3, 4},
-        {NULL, "8",     GDK_KEY_8,          GDK_KEY_KP_8, 1, 2, 3, 4},
-        {NULL, "9",     GDK_KEY_9,          GDK_KEY_KP_9, 2, 3, 3, 4},
-        {NULL, "×",     GDK_KEY_asterisk,   GDK_KEY_KP_Multiply, 3, 4, 3, 4},
-        {NULL, ",",     GDK_KEY_comma,      GDK_KEY_KP_Decimal, 0, 1, 4, 5},
-        {NULL, "0",     GDK_KEY_0,          GDK_KEY_KP_0, 1, 2, 4, 5},
-        {NULL, "=",     GDK_KEY_Return,     GDK_KEY_KP_Enter, 2, 3, 4, 5},
-        {NULL, "÷",     GDK_KEY_slash,      GDK_KEY_KP_Divide, 3, 4, 4, 5},
-};
-
 #define NUM_ROW_KEY_SCIENTIFIC 1
 #define NUM_COLUMN_KEY_SCIENTIFIC 4
-Key scientific_buttons[] = {
-        {NULL, "!", GDK_KEY_BackSpace, KEY_UNDECLARED, 0, 1, 0, 1}, //TODO GDK key
-        {NULL, "x²", GDK_KEY_BackSpace, KEY_UNDECLARED, 1, 2, 0, 1},
-        {NULL, "√", GDK_KEY_BackSpace, KEY_UNDECLARED, 2, 3, 0, 1},
-        {NULL, "log", GDK_KEY_BackSpace, KEY_UNDECLARED, 3, 4, 0, 1},
-};
-
-
 #define NUM_ROW_KEY_PROGRAMMER 1
 #define NUM_COLUMN_KEY_PROGRAMMER 4
-Key programmer_buttons[] = {
-        {NULL, "AND", GDK_KEY_BackSpace, KEY_UNDECLARED, 0, 1, 0, 1}, //TODO GDK key
-        {NULL, "OR", GDK_KEY_BackSpace, KEY_UNDECLARED, 1, 2, 0, 1},
-        {NULL, "NOT", GDK_KEY_BackSpace, KEY_UNDECLARED, 2, 3, 0, 1},
-        {NULL, "XOR", GDK_KEY_BackSpace, KEY_UNDECLARED, 3, 4, 0, 1},
+Key buttons[] = {
+        {NULL, "↩",     GDK_KEY_BackSpace,  KEY_UNDECLARED,         BASIC,      CONSTANT,   0, 1, 0, 1},
+        {NULL, "CE",    KEY_CLEAR,          KEY_UNDECLARED,         BASIC,      CONSTANT,   1, 2, 0, 1},
+        {NULL, "(-1)",  KEY_NEGATIVE,       KEY_UNDECLARED,         BASIC,      CONSTANT,   3, 4, 0, 1},
+        {NULL, "1",     GDK_KEY_1,          GDK_KEY_KP_1,           BASIC,      CONSTANT,   0, 1, 1, 2},
+        {NULL, "2",     GDK_KEY_2,          GDK_KEY_KP_2,           BASIC,      CONSTANT,   1, 2, 1, 2},
+        {NULL, "3",     GDK_KEY_3,          GDK_KEY_KP_3,           BASIC,      CONSTANT,   2, 3, 1, 2},
+        {NULL, "+",     GDK_KEY_plus,       GDK_KEY_KP_Add,         BASIC,      BINARY,     3, 4, 1, 2},
+        {NULL, "4",     GDK_KEY_4,          GDK_KEY_KP_4,           BASIC,      CONSTANT,   0, 1, 2, 3},
+        {NULL, "5",     GDK_KEY_5,          GDK_KEY_KP_5,           BASIC,      CONSTANT,   1, 2, 2, 3},
+        {NULL, "6",     GDK_KEY_6,          GDK_KEY_KP_6,           BASIC,      CONSTANT,   2, 3, 2, 3},
+        {NULL, "-",     GDK_KEY_minus,      GDK_KEY_KP_Subtract,    BASIC,      BINARY,     3, 4, 2, 3},
+        {NULL, "7",     GDK_KEY_7,          GDK_KEY_KP_7,           BASIC,      CONSTANT,   0, 1, 3, 4},
+        {NULL, "8",     GDK_KEY_8,          GDK_KEY_KP_8,           BASIC,      CONSTANT,   1, 2, 3, 4},
+        {NULL, "9",     GDK_KEY_9,          GDK_KEY_KP_9,           BASIC,      CONSTANT,   2, 3, 3, 4},
+        {NULL, "×",     GDK_KEY_asterisk,   GDK_KEY_KP_Multiply,    BASIC,      BINARY,     3, 4, 3, 4},
+        {NULL, ",",     GDK_KEY_comma,      GDK_KEY_KP_Decimal,     BASIC,      CONSTANT,   0, 1, 4, 5},
+        {NULL, "0",     GDK_KEY_0,          GDK_KEY_KP_0,           BASIC,      CONSTANT,   1, 2, 4, 5},
+        {NULL, "=",     GDK_KEY_Return,     GDK_KEY_KP_Enter,       BASIC,      CONSTANT,   2, 3, 4, 5},
+        {NULL, "÷",     GDK_KEY_slash,      GDK_KEY_KP_Divide,      BASIC,      CONSTANT,   3, 4, 4, 5},
+        {NULL, "!",     KEY_FACTORIAL,      KEY_UNDECLARED,         SCIENTIFIC, UNARY,      0, 1, 0, 1},
+        {NULL, "x²",    KEY_POWER,          KEY_UNDECLARED,         SCIENTIFIC, UNARY,      1, 2, 0, 1},
+        {NULL, "√",     KEY_SQRT,           KEY_UNDECLARED,         SCIENTIFIC, UNARY,      2, 3, 0, 1},
+        {NULL, "log",   KEY_LOG,            KEY_UNDECLARED,         SCIENTIFIC, UNARY,      3, 4, 0, 1},
+        {NULL, "AND",   KEY_AND,            KEY_UNDECLARED,         PROGRAMMER, BINARY,     0, 1, 0, 1},
+        {NULL, "OR",    KEY_OR ,            KEY_UNDECLARED,         PROGRAMMER, BINARY,     1, 2, 0, 1},
+        {NULL, "NOT",   KEY_NOT,            KEY_UNDECLARED,         PROGRAMMER, UNARY,      2, 3, 0, 1},
+        {NULL, "XOR",   KEY_XOR,            KEY_UNDECLARED,         PROGRAMMER, BINARY,     3, 4, 0, 1},
 };
 
-gint operators[] = {GDK_KEY_plus, GDK_KEY_KP_Add,
-                    GDK_KEY_minus, GDK_KEY_KP_Subtract,
-                    GDK_KEY_asterisk, GDK_KEY_KP_Multiply,
-                    GDK_KEY_slash, GDK_KEY_KP_Divide};
-
-
-//enum Mode {BASIC, SCIENTIFIC, PROGRAMMER} mode; TODO delete
 enum Calculation {OPERAND1, OPERATOR, OPERAND2};
-
 enum Calculation calculation_state = OPERAND1;
 GtkWidget *output_operand1;
 GtkWidget *output_operator;
@@ -88,6 +87,16 @@ GtkWidget *input;
 
 void quitCB(GtkWidget *widget, gpointer data){
     gtk_main_quit();
+}
+
+Key* get_button(gint in) {
+    size_t num_keys = sizeof(buttons) / sizeof(Key);
+    for(int i = 0; i < num_keys; i++){
+        if (buttons[i].gdk_key == in || buttons[i].gdk_key2 == in){
+            return &buttons[i];
+        }
+    }
+    return NULL;
 }
 
 GtkWidget* get_gtk_container_by_name(GList *containers, gchar *name){
@@ -104,6 +113,7 @@ GtkWidget* get_gtk_container_by_name(GList *containers, gchar *name){
 
 
 void changeModeBasicCB(GtkWidget *widget, gpointer data) {
+    mode = BASIC;
 }
 
 void changeModeScientificCB(GtkWidget *widget, gpointer data){
@@ -117,6 +127,7 @@ void changeModeScientificCB(GtkWidget *widget, gpointer data){
     } else {
         gtk_widget_hide(container);
     }
+    mode = SCIENTIFIC;
 }
 
 void changeModeProgrammerCB(GtkWidget *widget, gpointer data) {
@@ -125,11 +136,36 @@ void changeModeProgrammerCB(GtkWidget *widget, gpointer data) {
 
     GtkWidget *container = get_gtk_container_by_name(containers, "table-programmer-buttons");
 
+    Key *key_comma = get_button(GDK_KEY_comma);
+    Key *key_multiply = get_button(GDK_KEY_asterisk);
+    Key *key_division = get_button(GDK_KEY_slash);
+
     if (state) {
         gtk_widget_show(container);
+        if(key_comma != NULL){
+            gtk_widget_hide(key_comma->button);
+        }
+        if(key_multiply != NULL){
+            gtk_widget_hide(key_multiply->button);
+        }
+        if(key_division != NULL){
+            gtk_widget_hide(key_division->button);
+        }
     } else {
         gtk_widget_hide(container);
+        if(key_comma != NULL){
+            gtk_widget_show(key_comma->button);
+        }
+        if(key_multiply != NULL){
+            gtk_widget_show(key_multiply->button);
+        }
+        if(key_division != NULL){
+            gtk_widget_show(key_division->button);
+        }
     }
+
+
+    mode = PROGRAMMER;
 }
 
 
@@ -167,16 +203,6 @@ bool is_number(const char* str){
     return true;
 }
 
-bool is_operator(gint c){
-    for (int i = 0; i < sizeof(operators)/sizeof(gint); i++){
-        if (c == operators[i] ) {
-            return true;
-        }
-    }
-
-    return false;
-}
-
 
 int float_to_string(double num, char **str){
     char buffer[SIZE_OF_BUFFER];
@@ -190,18 +216,45 @@ int float_to_string(double num, char **str){
     return EXIT_SUCCESS;
 }
 
-bool do_math(int operator, double operand1, double operand2, double **result){
+bool do_math(gint operator, double operand1, double operand2, double **result){
     bool fail = false;
     double res = 0;
     switch (operator){
-        case '+':
+        case GDK_KEY_plus:
             res = operand1 + operand2; break;
-        case '-':
+        case GDK_KEY_minus:
             res = operand1 - operand2; break;
-        case '*':
+        case GDK_KEY_asterisk:
             res = operand1 * operand2; break;
-        case '/':
+        case GDK_KEY_slash:
             res = operand1 / operand2; break;
+        case KEY_FACTORIAL:
+            res = 1;
+            if (operand1 < 0) {
+                fail = true;
+            }
+            else {
+                for (int i = 1; i <= operand1; i++) {
+                    res *= i;
+                };
+            }
+            break;
+        case KEY_POWER:
+            res = operand1 * operand1; break;
+        case KEY_SQRT:
+            res = sqrt(operand1);
+            break;
+        case KEY_LOG:
+            res = log(operand1);
+            break;
+        case KEY_AND:
+            res = (int) operand1 & (int) operand2; break;
+        case KEY_OR:
+            res = (int) operand1 | (int) operand2; break;
+        case KEY_NOT:
+            res = (int) !operand1; break;
+        case KEY_XOR:
+            res = (int) operand1 ^ (int) operand2; break;
         default:
             fail = true;
             break;
@@ -232,28 +285,32 @@ void calc_mashine(Key *key){
 
     switch(calculation_state){
         case OPERAND1:
-            if (is_operator(key->gdk_key) && strcmp(input_str, "") != 0){
+            if ((key->arity == BINARY || key->arity == UNARY) && strcmp(input_str, "") != 0){
                 operator = key->gdk_key;
                 operand1 = atof(input_str);
-                calculation_state = OPERATOR;
                 gtk_label_set_text(GTK_LABEL(output_operand1), input_str);
                 gtk_label_set_text(GTK_LABEL(output_operator), key->label);
                 gtk_label_set_text(GTK_LABEL(output_operand2), "");
                 gtk_entry_set_text(GTK_ENTRY(input), "");
+                calculation_state = OPERATOR;
             }
             break;
         case OPERATOR:
-            if (is_operator(key->gdk_key)){
+            if (key->arity == BINARY || key->arity == UNARY){
                 operator = key->gdk_key;
                 gtk_label_set_text(GTK_LABEL(output_operator), key->label);
                 gtk_entry_set_text(GTK_ENTRY(input), "");
                 break;
             }
 
-            if (is_number(input_str)){
+            if (is_number(input_str) && strcmp(input_str, "") != 0 ){
                 calculation_state = OPERAND2;
+                break;
             }
-            break;
+            Key *operator_key = get_button(operator);
+            if (!(operator_key->arity == UNARY && (key->gdk_key == GDK_KEY_Return || key->gdk_key2 == GDK_KEY_KP_Enter))){
+                break;
+            }
         case OPERAND2:
             if (key->gdk_key == GDK_KEY_Return || key->gdk_key2 == GDK_KEY_KP_Enter){
                 operand2 = atof(input_str);
@@ -267,7 +324,9 @@ void calc_mashine(Key *key){
                 }
                 float_to_string(*result, &result_str);
                 guint len = (guint) strlen(result_str);
-                gtk_label_set_text(GTK_LABEL(output_operand2), input_str);
+                if(get_button(operator)->arity == BINARY) {
+                    gtk_label_set_text(GTK_LABEL(output_operand2), input_str);
+                }
                 GtkEntryBuffer *buffer = gtk_entry_get_buffer(GTK_ENTRY(input));
                 gtk_entry_buffer_set_text(GTK_ENTRY_BUFFER(buffer), result_str, len);
                 g_signal_emit_by_name(input, "move-cursor", GTK_MOVEMENT_LOGICAL_POSITIONS, len, FALSE, NULL);
@@ -282,28 +341,16 @@ void calc_mashine(Key *key){
     free(result);
 }
 
-Key* get_button(gint in) {
-    size_t num_keys = sizeof(basic_buttons) / sizeof(Key);
-    for(int i = 0; i < num_keys; i++){
-        if (basic_buttons[i].gdk_key == in || basic_buttons[i].gdk_key2 == in){
-            return &basic_buttons[i];
-        }
-    }
-    return NULL;
-}
-
-
-
-
 
 void button_clickedCB(GtkWidget *widget, gpointer data){
     gint in = *(gint *) data;
     gchar *result_str;
     Key *key = get_button(in);
-    if (key == NULL){
+    if (key == NULL)
         return;
-    }
-    calc_mashine(key);
+
+    if(!gtk_widget_get_visible(key->button))
+        return;
 
     GtkEntryBuffer *buffer = gtk_entry_get_buffer(GTK_ENTRY(input));
     const gchar *text = gtk_entry_buffer_get_text(buffer);
@@ -342,6 +389,7 @@ void button_clickedCB(GtkWidget *widget, gpointer data){
         clearCB(NULL, NULL);
     }
 
+    calc_mashine(key);
 }
 
 
@@ -355,13 +403,17 @@ gboolean key_pressedCB(GtkWidget *widget, GdkEventKey *event){
     }
 
     button_clickedCB(widget, (gpointer) &in);
-    return FALSE;
+    return TRUE;
 }
 
 
-void create_buttons(GtkTable *t, Key *keys, size_t num_keys){
+void create_buttons(GtkTable *t, Key *keys, size_t num_keys, enum Mode mode){
     GtkWidget *b;
     for (int i = 0; i < num_keys; i++){
+        if (keys[i].mode != mode){
+            continue;
+        }
+
         b = gtk_button_new_with_mnemonic(keys[i].label);
         keys[i].button = b;
         gtk_table_attach_defaults(t, b,
@@ -464,15 +516,15 @@ void initialize_app() {
 
     // Buttons
     GtkWidget *table_basic_buttons = gtk_table_new(NUM_ROW_KEY_BASIC, NUM_COLUMN_KEY_BASIC, TRUE);
-    create_buttons(GTK_TABLE(table_basic_buttons), basic_buttons, sizeof(basic_buttons) / sizeof(Key));
+    create_buttons(GTK_TABLE(table_basic_buttons), buttons, sizeof(buttons) / sizeof(Key), BASIC);
     gtk_widget_set_name(table_basic_buttons, "table-basic-buttons");
 
     GtkWidget *table_scientific_buttons = gtk_table_new(NUM_ROW_KEY_SCIENTIFIC, NUM_COLUMN_KEY_SCIENTIFIC, TRUE);
-    create_buttons(GTK_TABLE(table_scientific_buttons), scientific_buttons, sizeof(scientific_buttons) / sizeof(Key));
+    create_buttons(GTK_TABLE(table_scientific_buttons), buttons, sizeof(buttons) / sizeof(Key), SCIENTIFIC);
     gtk_widget_set_name(table_scientific_buttons, "table-scientific-buttons");
 
     GtkWidget *table_programmer_buttons = gtk_table_new(NUM_ROW_KEY_PROGRAMMER, NUM_COLUMN_KEY_PROGRAMMER, TRUE);
-    create_buttons(GTK_TABLE(table_programmer_buttons), programmer_buttons, sizeof(programmer_buttons) / sizeof(Key));
+    create_buttons(GTK_TABLE(table_programmer_buttons), buttons, sizeof(buttons) / sizeof(Key), PROGRAMMER);
     gtk_widget_set_name(table_programmer_buttons, "table-programmer-buttons");
 
 
